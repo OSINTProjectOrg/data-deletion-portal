f8ab65ac9e5b7863f63567b6471334333bc55979
Marcus TIPLER <TiplerMF@cardiff.ac.uk>
2026-04-10 11:36:29 +0100

    Initial web configuration complete with test of app.py using templates.

--- src/app.py
+++ src/app.py
@@ -1,1 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Flask, Blueprint, render_template
+import os
+
+# ----------------------------------------------------------
+# Doing initial setup for certain aspects of the project.
+# ----------------------------------------------------------
+dir_path = os.path.dirname(os.path.realpath(__file__))
+app = Flask(__name__)
+
+
+@app.route('/')
+def indexPage():
+    return render_template('index.html')
+
+
+# ----------------------------------------------------------
+# Creates parameters for the application to run.
+# ----------------------------------------------------------
+if __name__ == '__main__':
+	app.run(
+          debug=False,
+          host='0.0.0.0',
+          port=8080
+          )
